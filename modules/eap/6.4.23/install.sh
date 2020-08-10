f096da423d5e57585ba62d60235813816c21b65c
Daniel Kreling <dkreling@redhat.com>
2020-08-10 17:31:35 +0200

    [CLOUD-3757] [6.4.x] Apply jbossweb SPMM-2013 fix

--- modules/eap/6.4.23/install.sh
+++ modules/eap/6.4.23/install.sh
@@ -5,3 +5,4 @@ set -e
 SOURCES_DIR=/tmp/artifacts/
 
 $JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SOURCES_DIR/jboss-eap-6.4.23-patch.zip"
+$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SOURCES_DIR/BZ-1861193.zip"
