d3b56abd69999107fef172b1d20c07f2822e7232
Daniel Kreling <dkreling@redhat.com>
2020-09-14 15:57:12 +0200

    [CLOUD-3810] [6.4.x] Apply jbossweb SPMM-2344 fix - BZ-1875177

--- modules/eap/6.4.23/install.sh
+++ modules/eap/6.4.23/install.sh
@@ -6,3 +6,4 @@ SOURCES_DIR=/tmp/artifacts/
 
 $JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SOURCES_DIR/jboss-eap-6.4.23-patch.zip"
 $JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SOURCES_DIR/BZ-1861193.zip"
+$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SOURCES_DIR/BZ1875177.zip"
